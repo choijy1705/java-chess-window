1fc7af9780a18add85d9ae6b29b70c4a4497e72e
cjy17 <cjy1705@naver.com>
2021-03-22 14:58:35 +0900

    feat : 게임 시작 메인 화면 출력 구현

--- src/main/java/chess/view/OutputView.java
+++ src/main/java/chess/view/OutputView.java
@@ -46,4 +46,10 @@ public class OutputView {
         System.out.printf(SCORE_FORMAT, whiteScore, blackScore);
     }
 
+    public static void printMainPage() {
+        System.out.println("> 체스 게임을 시작합니다.");
+        System.out.println("> 게임 시작 : start");
+        System.out.println("> 게임 종료 : end");
+        System.out.println("> 게임 이동 : move source위치 target위치 - 예. move b2 b3");
+    }
 }
